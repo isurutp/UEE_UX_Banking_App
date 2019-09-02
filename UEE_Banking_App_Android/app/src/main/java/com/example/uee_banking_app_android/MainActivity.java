89501a0faf3d29c0ecd6a7f83e9db65b38f2fee4
Isuru Perera <isuruthathsara.vit@gmail.com>
2019-09-02 11:16:36 +0530

    Started android studio project
    Signed-off-by: Isuru Perera <isuruthathsara.vit@gmail.com>

---
+++ UEE_Banking_App_Android/app/src/main/java/com/example/uee_banking_app_android/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.uee_banking_app_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void login(View view) {
+        Intent intent = new Intent(this,sms_verification.class);
+        startActivity(intent);
+    }
+
+}
