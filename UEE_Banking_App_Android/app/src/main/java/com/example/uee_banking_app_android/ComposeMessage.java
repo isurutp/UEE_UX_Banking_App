ed3844e1b49f517e7ffe0f298d04891330336edd
Shehan Jiffry <dsjiffry@gmail.com>
2019-09-03 14:09:15 +0530

    Added set of UI's
    Signed-off-by: Shehan Jiffry <dsjiffry@gmail.com>

---
+++ UEE_Banking_App_Android/app/src/main/java/com/example/uee_banking_app_android/ComposeMessage.java
@@ -0,0 +1,14 @@
+package com.example.uee_banking_app_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ComposeMessage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_compose_message);
+    }
+}
