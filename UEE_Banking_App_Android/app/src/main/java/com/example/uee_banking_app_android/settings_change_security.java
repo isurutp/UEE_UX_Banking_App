21dd7da8235667d6b6362cffecc9bcd82a8149d4
Shehan Jiffry <dsjiffry@gmail.com>
2019-09-14 10:46:01 +0530

    Added settings control to shared pref
    Signed-off-by: Shehan Jiffry <dsjiffry@gmail.com>

--- UEE_Banking_App_Android/app/src/main/java/com/example/uee_banking_app_android/settings_change_security.java
+++ UEE_Banking_App_Android/app/src/main/java/com/example/uee_banking_app_android/settings_change_security.java
@@ -3,20 +3,78 @@ package com.example.uee_banking_app_android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Switch;
+import android.widget.Toast;
 
 public class settings_change_security extends AppCompatActivity {
+
+    private Switch email;
+    private Switch sms;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings_change_security);
+
+        email = findViewById(R.id.emailSwitch);
+        sms = findViewById(R.id.smsSwitch);
+
+        SharedPreferences prefs = getSharedPreferences("Settings", MODE_PRIVATE);
+        if (prefs.getInt("emailVerification",0) == 1)
+        {
+            email.setChecked(true);
+        }
+        if (prefs.getInt("smsVerification",0) == 1)
+        {
+            sms.setChecked(true);
+        }
+
     }
 
     public void mainMenu(View view){
         Intent intent = new Intent(this,menu_screen.class);
         startActivity(intent);
     }
+
+    public void update(View virw)
+    {
+        SharedPreferences.Editor editor = getSharedPreferences("Settings", MODE_PRIVATE).edit();
+        if(email.isChecked())
+        {
+            editor.putInt("emailVerification", 1);
+        }
+        else
+        {
+            editor.putInt("emailVerification", 0);
+        }
+
+        if(sms.isChecked())
+        {
+            editor.putInt("smsVerification", 1);
+        }
+        else
+        {
+            editor.putInt("smsVerification", 0);
+        }
+        editor.apply();
+        Toast.makeText(getApplicationContext(), "Settings Updated", Toast.LENGTH_LONG).show();
+    }
+
+
+    public void onEmailVerify(View V)
+    {
+        sms.setChecked(false);
+    }
+
+    public void onSmsVerify(View V)
+    {
+        email.setChecked(false);
+    }
+
 
 }
