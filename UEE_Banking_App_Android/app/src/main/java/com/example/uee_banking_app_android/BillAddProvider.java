629dec109a0e8ad9600b7af113f598b4239ef213
abhishek-kumar98 <abhishekkumar.best@gmail.com>
2019-09-15 17:15:26 +0530

    Signed-off-by: abhishek-kumar98 <abhishekkumar.best@gmail.com>

--- UEE_Banking_App_Android/app/src/main/java/com/example/uee_banking_app_android/BillAddProvider.java
+++ UEE_Banking_App_Android/app/src/main/java/com/example/uee_banking_app_android/BillAddProvider.java
@@ -1,5 +1,8 @@
 package com.example.uee_banking_app_android;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,16 +10,25 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class BillAddProvider extends Fragment {
 
+    Context mContext;
 
     public BillAddProvider() {
         // Required empty public constructor
     }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        mContext=context;
+    }
+
 
 
     @Override
@@ -24,6 +36,11 @@ public class BillAddProvider extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_bill_add, container, false);
+    }
+
+    public void addProviderButton(View view){
+        Intent intent = new Intent(mContext, add_provider_ui.class);
+        startActivity(intent);
     }
 
 }
