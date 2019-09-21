7d8df9d8da7c2874226294723dd800a19f4ffdcd
shahen <shahen.dedigama@gmail.com>
2019-09-22 02:17:50 +0530

    "fixed account"

--- UEE_Banking_App_Android/app/src/main/java/com/example/uee_banking_app_android/TransactionHistory.java
+++ UEE_Banking_App_Android/app/src/main/java/com/example/uee_banking_app_android/TransactionHistory.java
@@ -13,19 +13,54 @@ import java.util.ArrayList;
 public class TransactionHistory extends AppCompatActivity {
 
 
-    ArrayList<String> type = new ArrayList<>();
-    ArrayList<String>  payment = new ArrayList<>();
-    ArrayList<String>  amount = new ArrayList<>();
-    ArrayList<String>  balance = new ArrayList<>();
-    ArrayList<String>  date = new ArrayList<>();
+    ArrayList<String> type ;
+    ArrayList<String>  payment;
+    ArrayList<String>  amount ;
+    ArrayList<String>  balance;
+    ArrayList<String>  date;
+    ArrayList<Number>  positions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transaction_history);
+        init();
         initValues();
         initRecyclerView();
     }
+
+    private void init(){
+        type = new ArrayList<>();
+        payment = new ArrayList<>();
+        amount = new ArrayList<>();
+        balance = new ArrayList<>();
+        date = new ArrayList<>();
+        positions = new ArrayList<>();
+    }
+
+
+    private void filter(){
+
+    }
+
+    private void setFilterPosition(String key){
+        ArrayList positions = new ArrayList<Number>();
+        for(int i = 0; i >= type.size(); i++){
+            if(type.get(i).matches("(.*)"+ key +"(.*)")||
+            payment.get(i).matches("(.*)"+ key +"(.*)")  ||
+            balance.get(i).matches("(.*)"+ key +"(.*)")  ||
+            amount.get(i).matches("(.*)"+ key +"(.*)") ||
+            date.get(i).matches("(.*)"+ key +"(.*)")){
+                positions.add(i);
+            }
+        }
+        this.positions = positions;
+    }
+
+    private void alterArray(){
+
+    }
+
 
 
     private void initValues(){
@@ -36,7 +71,52 @@ public class TransactionHistory extends AppCompatActivity {
         balance.add("234,678");
         date.add("12/12/19");
         type.add("ATM");
+        payment.add("Deposited");
+        amount.add("20,000");
+        balance.add("234,678");
+        date.add("12/12/19");
+        type.add("Credit");
         payment.add("Withdrawal");
+        amount.add("20,000");
+        balance.add("234,678");
+        date.add("12/12/19");
+        type.add("Transfer");
+        payment.add("Deposited");
+        amount.add("20,000");
+        balance.add("234,678");
+        date.add("12/12/19");
+        type.add("ATM");
+        payment.add("Withdrawal");
+        amount.add("20,000");
+        balance.add("234,678");
+        date.add("12/12/19");
+        type.add("Debit");
+        payment.add("Deposited");
+        amount.add("20,000");
+        balance.add("234,678");
+        date.add("12/12/19");
+        type.add("ATM");
+        payment.add("Withdrawal");
+        amount.add("20,000");
+        balance.add("234,678");
+        date.add("12/12/19");
+        type.add("Payment");
+        payment.add("Deposited");
+        amount.add("20,000");
+        balance.add("234,678");
+        date.add("12/12/19");
+        type.add("ATM");
+        payment.add("Deposited");
+        amount.add("20,000");
+        balance.add("234,678");
+        date.add("12/12/19");
+        type.add("Transfer");
+        payment.add("Withdrawal");
+        amount.add("20,000");
+        balance.add("234,678");
+        date.add("12/12/19");
+        type.add("ATM");
+        payment.add("Deposited");
         amount.add("20,000");
         balance.add("234,678");
         date.add("12/12/19");
@@ -50,17 +130,12 @@ public class TransactionHistory extends AppCompatActivity {
         amount.add("20,000");
         balance.add("234,678");
         date.add("12/12/19");
-        type.add("ATM");
+        type.add("Credit");
         payment.add("Withdrawal");
         amount.add("20,000");
         balance.add("234,678");
         date.add("12/12/19");
-        type.add("Debit");
-        payment.add("Withdrawal");
-        amount.add("20,000");
-        balance.add("234,678");
-        date.add("12/12/19");
-        type.add("ATM");
+        type.add("Credit");
         payment.add("Withdrawal");
         amount.add("20,000");
         balance.add("234,678");
@@ -71,10 +146,301 @@ public class TransactionHistory extends AppCompatActivity {
         balance.add("234,678");
         date.add("12/12/19");
         type.add("ATM");
+        payment.add("Deposited");
+        amount.add("20,000");
+        balance.add("234,678");
+        date.add("12/12/19");
+        type.add("Transfer");
         payment.add("Withdrawal");
         amount.add("20,000");
         balance.add("234,678");
         date.add("12/12/19");
+        type.add("Payment");
+        payment.add("Withdrawal");
+        amount.add("20,000");
+        balance.add("234,678");
+        date.add("12/12/19");
+        type.add("Debit");
+        payment.add("Withdrawal");
+        amount.add("20,000");
+        balance.add("234,678");
+        date.add("12/12/19");
+        type.add("Payment");
+        payment.add("Withdrawal");
+        amount.add("20,000");
+        balance.add("234,678");
+        date.add("12/12/19");
+        type.add("ATM");
+        payment.add("Deposited");
+        amount.add("20,000");
+        balance.add("234,678");
+        date.add("12/12/19");
+        type.add("Debit");
+        payment.add("Deposited");
+        amount.add("20,000");
+        balance.add("234,678");
+        date.add("12/12/19");
+        type.add("ATM");
+        payment.add("Deposited");
+        amount.add("20,000");
+        balance.add("234,678");
+        date.add("12/12/19");
+        type.add("Credit");
+        payment.add("Withdrawal");
+        amount.add("20,000");
+        balance.add("234,678");
+        date.add("12/12/19");
+        type.add("Transfer");
+        payment.add("Deposited");
+        amount.add("20,000");
+        balance.add("234,678");
+        date.add("12/12/19");
+        type.add("ATM");
+        payment.add("Withdrawal");
+        amount.add("20,000");
+        balance.add("234,678");
+        date.add("12/12/19");
+        type.add("Debit");
+        payment.add("Deposited");
+        amount.add("20,000");
+        balance.add("234,678");
+        date.add("12/12/19");
+        type.add("ATM");
+        payment.add("Withdrawal");
+        amount.add("20,000");
+        balance.add("234,678");
+        date.add("12/12/19");
+        type.add("Payment");
+        payment.add("Deposited");
+        amount.add("20,000");
+        balance.add("234,678");
+        date.add("12/12/19");
+        type.add("ATM");
+        payment.add("Deposited");
+        amount.add("20,000");
+        balance.add("234,678");
+        date.add("12/12/19");
+        type.add("Transfer");
+        payment.add("Withdrawal");
+        amount.add("20,000");
+        balance.add("234,678");
+        date.add("12/12/19");
+        type.add("ATM");
+        payment.add("Deposited");
+        amount.add("20,000");
+        balance.add("234,678");
+        date.add("12/12/19");
+        type.add("Credit");
+        payment.add("Withdrawal");
+        amount.add("20,000");
+        balance.add("234,678");
+        date.add("12/12/19");
+        type.add("Transfer");
+        payment.add("Withdrawal");
+        amount.add("20,000");
+        balance.add("234,678");
+        date.add("12/12/19");
+        type.add("Credit");
+        payment.add("Withdrawal");
+        amount.add("20,000");
+        balance.add("234,678");
+        date.add("12/12/19");
+        type.add("Credit");
+        payment.add("Withdrawal");
+        amount.add("20,000");
+        balance.add("234,678");
+        date.add("12/12/19");
+        type.add("Payment");
+        payment.add("Withdrawal");
+        amount.add("20,000");
+        balance.add("234,678");
+        date.add("12/12/19");
+        type.add("ATM");
+        payment.add("Deposited");
+        amount.add("20,000");
+        balance.add("234,678");
+        date.add("12/12/19");
+        type.add("Transfer");
+        payment.add("Withdrawal");
+        amount.add("20,000");
+        balance.add("234,678");
+        date.add("12/12/19");
+        type.add("ATM");
+        payment.add("Deposited");
+        amount.add("20,000");
+        balance.add("234,678");
+        date.add("12/12/19");
+        type.add("Credit");
+        payment.add("Withdrawal");
+        amount.add("20,000");
+        balance.add("234,678");
+        date.add("12/12/19");
+        type.add("Transfer");
+        payment.add("Deposited");
+        amount.add("20,000");
+        balance.add("234,678");
+        date.add("12/12/19");
+        type.add("ATM");
+        payment.add("Withdrawal");
+        amount.add("20,000");
+        balance.add("234,678");
+        date.add("12/12/19");
+        type.add("Debit");
+        payment.add("Deposited");
+        amount.add("20,000");
+        balance.add("234,678");
+        date.add("12/12/19");
+        type.add("ATM");
+        payment.add("Withdrawal");
+        amount.add("20,000");
+        balance.add("234,678");
+        date.add("12/12/19");
+        type.add("Payment");
+        payment.add("Deposited");
+        amount.add("20,000");
+        balance.add("234,678");
+        date.add("12/12/19");
+        type.add("ATM");
+        payment.add("Deposited");
+        amount.add("20,000");
+        balance.add("234,678");
+        date.add("12/12/19");
+        type.add("Transfer");
+        payment.add("Withdrawal");
+        amount.add("20,000");
+        balance.add("234,678");
+        date.add("12/12/19");
+        type.add("ATM");
+        payment.add("Deposited");
+        amount.add("20,000");
+        balance.add("234,678");
+        date.add("12/12/19");
+        type.add("Credit");
+        payment.add("Withdrawal");
+        amount.add("20,000");
+        balance.add("234,678");
+        date.add("12/12/19");
+        type.add("Transfer");
+        payment.add("Withdrawal");
+        amount.add("20,000");
+        balance.add("234,678");
+        date.add("12/12/19");
+        type.add("Credit");
+        payment.add("Withdrawal");
+        amount.add("20,000");
+        balance.add("234,678");
+        date.add("12/12/19");
+        type.add("Credit");
+        payment.add("Withdrawal");
+        amount.add("20,000");
+        balance.add("234,678");
+        date.add("12/12/19");
+        type.add("Payment");
+        payment.add("Withdrawal");
+        amount.add("20,000");
+        balance.add("234,678");
+        date.add("12/12/19");
+        type.add("ATM");
+        payment.add("Deposited");
+        amount.add("20,000");
+        balance.add("234,678");
+        date.add("12/12/19");
+        type.add("Transfer");
+        payment.add("Withdrawal");
+        amount.add("20,000");
+        balance.add("234,678");
+        date.add("12/12/19");
+        type.add("ATM");
+        payment.add("Deposited");
+        amount.add("20,000");
+        balance.add("234,678");
+        date.add("12/12/19");
+        type.add("Credit");
+        payment.add("Withdrawal");
+        amount.add("20,000");
+        balance.add("234,678");
+        date.add("12/12/19");
+        type.add("Transfer");
+        payment.add("Deposited");
+        amount.add("20,000");
+        balance.add("234,678");
+        date.add("12/12/19");
+        type.add("ATM");
+        payment.add("Withdrawal");
+        amount.add("20,000");
+        balance.add("234,678");
+        date.add("12/12/19");
+        type.add("Debit");
+        payment.add("Deposited");
+        amount.add("20,000");
+        balance.add("234,678");
+        date.add("12/12/19");
+        type.add("ATM");
+        payment.add("Withdrawal");
+        amount.add("20,000");
+        balance.add("234,678");
+        date.add("12/12/19");
+        type.add("Payment");
+        payment.add("Deposited");
+        amount.add("20,000");
+        balance.add("234,678");
+        date.add("12/12/19");
+        type.add("ATM");
+        payment.add("Deposited");
+        amount.add("20,000");
+        balance.add("234,678");
+        date.add("12/12/19");
+        type.add("Transfer");
+        payment.add("Withdrawal");
+        amount.add("20,000");
+        balance.add("234,678");
+        date.add("12/12/19");
+        type.add("ATM");
+        payment.add("Deposited");
+        amount.add("20,000");
+        balance.add("234,678");
+        date.add("12/12/19");
+        type.add("Credit");
+        payment.add("Withdrawal");
+        amount.add("20,000");
+        balance.add("234,678");
+        date.add("12/12/19");
+        type.add("Transfer");
+        payment.add("Withdrawal");
+        amount.add("20,000");
+        balance.add("234,678");
+        date.add("12/12/19");
+        type.add("Credit");
+        payment.add("Withdrawal");
+        amount.add("20,000");
+        balance.add("234,678");
+        date.add("12/12/19");
+        type.add("Credit");
+        payment.add("Withdrawal");
+        amount.add("20,000");
+        balance.add("234,678");
+        date.add("12/12/19");
+        type.add("Payment");
+        payment.add("Withdrawal");
+        amount.add("20,000");
+        balance.add("234,678");
+        date.add("12/12/19");
+        type.add("ATM");
+        payment.add("Deposited");
+        amount.add("20,000");
+        balance.add("234,678");
+        date.add("12/12/19");
+        type.add("Transfer");
+        payment.add("Withdrawal");
+        amount.add("20,000");
+        balance.add("234,678");
+        date.add("12/12/19");
+
+
+
+
+
+
 
 
     }
@@ -86,4 +452,14 @@ public class TransactionHistory extends AppCompatActivity {
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
+
+    private void filterRecyclerView() {
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        RecycerViewAdapterTHistory adapter = new RecycerViewAdapterTHistory(this, type,
+                payment, amount, balance, date);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+
 }
